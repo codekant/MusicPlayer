7366b721433555f124b7bbfa7c1723089225f399
srrrrkant <53932812+srrrrkant@users.noreply.github.com>
2020-02-23 17:26:03 +0530

    bug fix #2
    just that some songs were not playing due to copyright protection and stuff on youtube, soo....

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 const ejs = require("ejs");
-const express = require('express');
+const express = require("express");
 const ytdl = require("ytdl-core");
 const http = require("http");
 const app = express();
 
 let server = http.createServer(app);
 server.listen(3000, () => {
-    console.log("Listening on PORT 3000");
+  console.log("Listening on PORT 3000");
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static('bootstrap'));
+app.set("view engine", "ejs");
+app.use(express.static("bootstrap"));
 
-app.get("/", async(req, res) => {
-    res.render("input", {
-        error: false
-    });
+app.get("/", async (req, res) => {
+  res.render("input", {
+    error: false,
+    bitError: false
+  });
 });
 
-app.get("/play", async(req, res) => {
-    let url = req.query.url;
-    if(!url) return res.render("input", {
-        error: true
+app.get("/play", async (req, res) => {
+  let url = req.query.url;
+  if (!url)
+    return res.render("input", {
+      error: true,
+      bitError: false
     });
-    if(!url.includes("youtube.com") && !url.includes("youtu.be")) 
-           return res.render("input", {
-        error: true
+  if (!url.includes("youtube.com") && !url.includes("youtu.be"))
+    return res.render("input", {
+      error: true
     });
-    await ytdl.getInfo(url, (err, pog) => {
-        if(err) {
-            return res.render("output", {
-                error: true
-            });
-        } else {
-            res.render("output", {
-                title: pog.title,
-                thumbnail: `https://i.ytimg.com/vi/${pog.video_id}/maxresdefault.jpg`,
-                play: pog.formats[0].url,
-                loop: req.query.loop ? req.query.loop : "false",
-                data: pog
-            });
-        }
-    })
+  await ytdl.getInfo(url, (err, pog) => {
+    if (err) {
+      return res.render("output", {
+        error: true,
+        bitError: false
+      });
+    } else {
+      let song = pog.formats.find(o => 500000 > o.bitrate);
+      if (!song) {
+        return res.render("input", {
+          bitError: true,
+          error: false
+        });
+      }
+      res.render("output", {
+        title: pog.title,
+        thumbnail: `https://i.ytimg.com/vi/${pog.video_id}/maxresdefault.jpg`,
+        play: song.url,
+        loop: req.query.loop ? req.query.loop : "false",
+        data: pog
+      });
+    }
+  });
 });
